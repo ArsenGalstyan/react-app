91f9f588e64bd707447caf232c76bc2cfdab0a62
Arsen <arsengalstyan29826@gmail.com>
2021-04-24 11:16:22 +0400

    skzbnakan" ;

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import fun from './lib.js'
+import './App.css';
+
+function App() {
+  fun("Arsen")
+  return (
+    
+    <div className="App">
+        
+        
+    </div>
+  );
+}
+
+export default App;
