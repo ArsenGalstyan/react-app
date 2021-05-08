8b5e971b60788daa5186ae963f9428fcf3af3c9e
Arsen <arsengalstyan29826@gmail.com>
2021-05-08 10:39:57 +0400

    das5na-inq@

--- src/comp/Test2.jsx
+++ src/comp/Test2.jsx
@@ -1,25 +1,32 @@
 import react from 'react'
 import zapros from '../provaider/zaprosner'
 import url from '../provaider/Constants'
+import {Load} from './loading'
 
 class Test2 extends react.Component {
     constructor(props) {
         super(props)
 
         this.state = {
-            users: []
+            users: [],
+            loaded: false
         }
     }
 
     async componentDidMount() {
         const users = await zapros("GET", url + `users`);
-        this.setState({ users })
+        this.setState({ users , loaded : true})
     }
     render() {
 
         return (
             <div>
-                {this.state.users.map((user,i)=> <p key={i}>{user.name}</p> )}
+                {
+                    this.state.loaded ?
+                        this.state.users.map((user, i) => <p key={i}>{user.name}</p>)
+                        :
+                        <Load />
+                }
             </div>
         )
     }
---
+++ src/comp/loading.jsx
@@ -0,0 +1,13 @@
+import Loader from "react-loader-spinner";
+
+
+export  function Load() {
+        return (
+          <Loader
+            type="Puff"
+            color="#00BFFF"
+            height={100}
+            width={100}
+          />
+        );
+}
